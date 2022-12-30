97b84565a58c70a9cc8fb6da3f38b98c531f9ef3
TomTheCattt <100224706+TomTheCattt@users.noreply.github.com>
2022-12-30 19:53:27 +0700

    Add files via upload

---
+++ hehe/src/hehe/Hehe.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hehe;
+
+/**
+ *
+ * @author VIet Anh
+ */
+public class Hehe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void Main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
